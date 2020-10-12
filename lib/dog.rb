3a785ab3f9a74999ff6695db9d8bed87201eb950
ChrisDemahy <demahyc@gmail.com>
2020-10-12 12:33:08 -0500

    finished this lab

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,40 @@
-# Add your code here
+# Describes a dog
+class Dog
+
+### CLASS VARIABLES AND METHODS ###
+
+  # class variables
+  @@all = []
+
+  # Class method to access variables
+  def self.all
+    @@all
+  end
+
+  # Class method to print all dogs
+  def self.print_all
+    self.all.each{|e| puts e.name} # puts each dog
+  end
+
+  # Class method to clear all dogs from all class variable
+  def self.clear_all
+    self.all.clear
+  end
+
+### INSTANCE VARIABLES AND METHODS ###
+
+  # instance readrs and writers
+  attr_accessor :name
+
+  # default class constructor
+  def initialize(name=nil)
+    self.name = name
+    self.save
+  end
+
+  # Push self onto the all class variable
+  def save
+    self.class.all << self
+  end
+
+end
